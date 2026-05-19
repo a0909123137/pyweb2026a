8e060a01830d36e4e8d77ebf72306c5499089dba
a0909123137 <a0909123137@gmail.com>
2026-05-19 16:26:19 +0800

    聊天機器人

--- web.py
+++ web.py
@@ -38,7 +38,12 @@ def index():
     link += "<a href='/road'>查詢易肇事路口</a><hr>"
     link += "<a href='/weather'>天氣查詢</a><hr>"
     link += "<a href='/rate'>本週新片DB更新</a><hr>"
+    link += "<a href='/demo'>聊天機器人</a><hr>"
     return link
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
 
 @app.route("/webhook3", methods=["POST"])
 def webhook3():
